d1d10a5c145e59c6e2bf809c1ad2f54ea446c061
SAYRUS1 <slepper8@gmail.com>
2023-07-13 03:41:48 +0300

    Startup file Cal_v1.1.py

---
+++ lib_if_elif.py
@@ -0,0 +1,28 @@
+def sump(a,b,c):
+    if c == "+":
+        d = a + b
+        print(f"{a + b }")
+    else:
+        summ(a,b,c)
+def summ(a,b,c):
+    if c == "-":
+        d = a - b
+        print(f"{a - b }")
+    else:
+        sumu(a,b,c)
+def sumu(a,b,c):
+    if c == "*":
+        d = a * b
+        print(f"{a * b }")
+    else:
+        sumr(a,b,c)
+def sumr(a,b,c):
+    if c == "/":
+        d = a / b
+        print(f"{a / b }")
+    else:
+        Fuck = '''Ого, если вы видите это сообщение 
+        то мы пока не знаем как это произошло и
+         уже начали разбираться в чём тут дело     (нет)
+                                         + но вы всё равно напишите нам)'''
+        print(Fuck)
